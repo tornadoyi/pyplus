25640d8ab770003f125ed891f35c33a304f8de57
Gu sir <390512308@qq.com>
2016-11-30 20:05:30 +0800

    fix qdict

--- pyplus/qdict.py
+++ pyplus/qdict.py
@@ -1,7 +1,22 @@
 
 class qdict(dict):
+    def __init__(self, *args, **kwargs):
+        for arg in args:
+            d = None
+            if type(arg) == dict:
+                d = arg
+            elif hasattr(arg, '__dict__'):
+                d = arg.__dict__
+            else:
+                continue
+            for (k, v) in d.items():
+                self.__setitem__(k, v)
+
+        for (k, v) in kwargs.items():
+            self.__setitem__(k, v)
+
     def __getattr__(self, item):
         return dict.get(self, item, None)
 
-    def __setitem__(self, key, value):
-        self.setdefault(key, value)
+    def __setattr__(self, key, value):
+        dict.__setitem__(self, key, value)
